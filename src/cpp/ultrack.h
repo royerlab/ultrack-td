35ed2ff8ec9d07637ebd665ba82ee60be9219af3
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-11-09 18:16:14 -0800

    wip

--- src/cpp/ultrack.h
+++ src/cpp/ultrack.h
@@ -1,3 +1,5 @@
+#include <vector>
+#include <cstring>
 #include <nanobind/ndarray.h>
 #include <nanobind/nanobind.h>
 
@@ -7,13 +9,47 @@ using namespace nb::literals;
 
 void inspect(const nb::ndarray<>& a);
 
+struct Segment {
+    nb::ndarray<bool> mask;
+    nb::ndarray<int> bbox;
+    int num_pixels;
+    int z;
+    int y;
+    int x;
+};
+
+
 template <typename T>
-void compute_segmentation_hypotheses(
+std::vector<Segment> compute_segmentation_hypotheses(
     const nb::ndarray<bool>& foreground,
     const nb::ndarray<T>& contours,
     int min_num_pixels,
     int max_num_pixels,
     float min_frontier
 ) {
-    printf("Computing segmentation hypotheses...\n");
+    size_t depth = foreground.shape(0);
+    size_t height = foreground.shape(1);
+    size_t width = foreground.shape(2);
+
+    bool *seen_data = new bool[depth * height * width];
+    std::memset(seen_data, 0, depth * height * width * sizeof(bool));
+
+    bool *fg_data = foreground.data();
+    T *ctr_data = contours.data();
+
+    auto seen = nb::ndarray<nb::numpy, bool, nb::ndim<2>>(seen_data, {depth, height, width});
+
+    std::vector<Segment> segments;
+
+    for (int z = 0; z < depth; z++) {
+        for (int y = 0; y < height; y++) {
+            for (int x = 0; x < width; x++) {
+                if (fg_data[z * height * width + y * width + x]) {
+                    //  segments.push_back(Segment(foreground, z, y, x));
+                }
+            }
+        }
+    }
+
+    return segments;
 }
--- src/ultrack_td_ext.cpp
+++ src/ultrack_td_ext.cpp
@@ -1,5 +1,6 @@
 #include <nanobind/nanobind.h>
 #include <nanobind/ndarray.h>
+#include <nanobind/stl/vector.h>
 #include "cpp/ultrack.h"
 
 namespace nb = nanobind;
@@ -9,6 +10,16 @@ using namespace nb::literals;
 NB_MODULE(ultrack_td_ext, m) {
     m.doc() = "This is a \"hello world\" example with nanobind";
     m.def("inspect", inspect, "a"_a);
+
+    nb::class_<Segment>(m, "Segment")
+    .def(nb::init<nb::ndarray<bool>, nb::ndarray<int>, int, int, int>())
+    .def_ro("mask", &Segment::mask)
+    .def_ro("bbox", &Segment::bbox)
+    .def_ro("num_pixels", &Segment::num_pixels)
+    .def_ro("z", &Segment::z)
+    .def_ro("y", &Segment::y)
+    .def_ro("x", &Segment::x);
+
     m.def("compute_segmentation_hypotheses", compute_segmentation_hypotheses<float>, "foreground"_a, "contours"_a, "min_num_pixels"_a, "max_num_pixels"_a, "min_frontier"_a);
     // TODO other types
 }
