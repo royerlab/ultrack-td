381b1d94b2f4cb32fd41f4a96b1c5a4d05f0afdd
Jordao Bragantini <jordao.bragantini@czbiohub.org>
2025-11-09 18:38:25 -0800

    adding connected component function

--- src/cpp/ultrack.h
+++ src/cpp/ultrack.h
@@ -20,6 +20,61 @@ struct Segment {
 
 
 template <typename T>
+void compute_connected_components(
+    std::vector<Segment> &segments,
+    const bool *fg_data,
+    const T *ctr_data,
+    bool *seen_data,
+    int depth,
+    int height,
+    int width,
+    int min_num_pixels,
+    int max_num_pixels,
+    float min_frontier,
+    int cur_idx
+) {
+    std::vector<int> queue = {cur_idx};
+    std::vector<int> visited;
+
+    int offsets[18] = {
+        0, 0, 1,
+        0, 1, 0,
+        1, 0, 0,
+        0, -1, 0,
+        0, 0, -1,
+        -1, 0, 0,
+    };
+
+    while (!queue.empty())
+    {
+        int idx = queue.back();
+        queue.pop_back();
+        seen_data[idx] = true;
+        visited.push_back(idx);
+        int cur_z = idx / (height * width);
+        int cur_y = (idx % (height * width)) / width;
+        int cur_x = idx % width;
+        for (int i = 0; i < 6; i++) {
+            int nz = cur_z + offsets[i * 3];
+            int ny = cur_y + offsets[i * 3 + 1];
+            int nx = cur_x + offsets[i * 3 + 2];
+            if (
+                nz >= 0 && nz < depth &&
+                ny >= 0 && ny < height &&
+                nx >= 0 && nx < width
+            ) {
+                int nidx = nz * height * width + ny * width + nx;
+                if (!seen_data[nidx]) {
+                    seen_data[nidx] = true;
+                    queue.push_back(nidx);
+                }
+            }
+        }
+    }
+}
+
+
+template <typename T>
 std::vector<Segment> compute_segmentation_hypotheses(
     const nb::ndarray<bool>& foreground,
     const nb::ndarray<T>& contours,
@@ -37,15 +92,20 @@ std::vector<Segment> compute_segmentation_hypotheses(
     bool *fg_data = foreground.data();
     T *ctr_data = contours.data();
 
-    auto seen = nb::ndarray<nb::numpy, bool, nb::ndim<2>>(seen_data, {depth, height, width});
-
     std::vector<Segment> segments;
 
     for (int z = 0; z < depth; z++) {
+        int z_step = z * height * width;
         for (int y = 0; y < height; y++) {
+            int y_step = y * width;
             for (int x = 0; x < width; x++) {
-                if (fg_data[z * height * width + y * width + x]) {
-                    //  segments.push_back(Segment(foreground, z, y, x));
+                int idx = z_step + y_step + x;
+                if (fg_data[idx] && !seen_data[idx]) {
+                    compute_connected_components(
+                        segments, fg_data, ctr_data, seen_data,
+                        depth, height, width,
+                        min_num_pixels, max_num_pixels, min_frontier, idx
+                    );
                 }
             }
         }
